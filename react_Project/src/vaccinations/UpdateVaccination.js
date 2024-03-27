b598babc5d481e3ee0de24c47bcc0af34f413a81
miriMayevsky <151126542+miriMayevsky@users.noreply.github.com>
2024-03-27 16:49:31 +0200

    commit

--- react_Project/src/vaccinations/UpdateVaccination.js
+++ react_Project/src/vaccinations/UpdateVaccination.js
@@ -6,9 +6,10 @@ import { useNavigate } from 'react-router-dom';
 
 const UpdateVaccination = () => {
     let navigate = useNavigate();
-    let { register, handleSubmit, formState: { errors }, reset } = useForm();
-    const [Vaccinations, setVaccinations] = useState({});
+    let { register, handleSubmit, formState: { errors }, reset, setValue } = useForm();
+    const [Vaccinations, setVaccinations] = useState([]);
     let { idNumber, index } = useParams();
+    const [currentVaccin, SetCurrentVaccin] = useState({});
 
     useEffect(() => {
         GetById(idNumber)
@@ -25,16 +26,27 @@ const UpdateVaccination = () => {
     }, []);
 
     useEffect(() => {
-        if (Vaccinations[index]) {
-            reset(Vaccinations[index]);
-        }
-    }, [Vaccinations[index], reset]);
+        const vaccination = Vaccinations.find(v => v.index == index);
+        if (vaccination) {
+            SetCurrentVaccin(vaccination);
 
-console.log(Vaccinations[index]);
+        }
+    }, [Vaccinations, index]);
+
+    useEffect(() => {
+        if (currentVaccin) {
+            reset(currentVaccin);
+        }
+    }, [currentVaccin]);
+
+    console.log(index);
 
     const save = (data) => {
+        console.log(data);
         UpdateVaccinations(idNumber, index, data)
             .then((res) => {
+                console.log(index);
+
                 alert("  The details have been successfully updated  ");
                 reset();
                 navigate(`/userDetails/${idNumber}`);
@@ -46,31 +58,37 @@ console.log(Vaccinations[index]);
     };
 
     return (
-        // <div style={{ margin: "20px auto", maxWidth: "500px" }}>
         <div style={{ margin: "200px auto", maxWidth: "500px" }}>
-        <form onSubmit={handleSubmit(save)} style={{ display: "flex", flexDirection: "column" }}>
-                    <div style={{ marginBottom: "10px" }}></div>
-                   
-                    <label>date</label>
-                    <input {...register('date', { 
-                        required: 'Field is required',
-                        max: { value: new Date().toISOString().split('T')[0], message:' the date cannot be in the future' },
-                        min: { value: '2021-01-01', message: 'The date must be later than 2021-01-01' }
-                     })} type="date" />
-                    {errors.date && <p>{errors.date.message}</p>}
-                    <div style={{ marginBottom: "10px" }}></div>
-                    
-                    <select {...register('manufacturer', { required: 'Field is required' })} 
-                    value={Vaccinations[index]?.manufacturer}>
-                        <option value="fizer">Fizer</option>
-                        <option value="Moderna">Moderna</option>
-                    </select>
+            <form onSubmit={handleSubmit(save)} style={{ display: "flex", flexDirection: "column" }}>
+                <div style={{ marginBottom: "10px" }}></div>
 
-                    {errors.manufacturer && <p>{errors.manufacturer.message}</p>}
+                <label>date</label>
+                <input {...register('date', {
+                    required: 'Field is required',
+                    max: { value: new Date().toISOString().split('T')[0], message: ' the date cannot be in the future' },
+                    min: { value: '2021-01-01', message: 'The date must be later than 2021-01-01' }
+                })} type="date" />
+                {errors.date && <p>{errors.date.message}</p>}
+                <div style={{ marginBottom: "10px" }}></div>
 
-                    <input type="submit" style={{ marginTop: "20px", textAlign: "center", display: "block" }} />
-                </form>
-            {/* </div> */}
+
+                <select
+                    {...register('manufacturer', { required: 'Field is required' })}
+                    value={currentVaccin?.manufacturer}
+                    onChange={(e) => {
+                        SetCurrentVaccin({ ...currentVaccin, manufacturer: e.target.value });
+                        setValue('manufacturer', e.target.value);
+                    }}
+                >
+                    <option value="fizer">Fizer</option>
+                    <option value="Moderna">Moderna</option>
+                </select>
+
+
+                {errors.manufacturer && <p>{errors.manufacturer.message}</p>}
+
+                <input type="submit" style={{ marginTop: "20px", textAlign: "center", display: "block" }} />
+            </form>
 
             <Link to={`/userDetails/${idNumber}`} style={{ marginTop: "20px", textAlign: "center", display: "block" }}>
                 back
